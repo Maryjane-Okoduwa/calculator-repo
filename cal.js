4e4211cc26b5953dc1ca34c006751404e9b3ec60
Maryjane-Okoduwa <105215555+Maryjane-Okoduwa@users.noreply.github.com>
2022-06-24 17:39:03 +0100

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,22 @@
+function addition(x,y){
+    let result = x + y
+    return result
+    
+}
+function substraction (x,y){
+    let result = x-y
+    return result
+}
+
+function multiplication (x,y){
+    let result = x*y
+    return result
+}
+
+function division (x,y){
+    let result = x/y
+    return result
+}
+
+
+console.log(division(4,2))
